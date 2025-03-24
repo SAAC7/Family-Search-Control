eaf6de3236213038104c9d246b76f15aa021c1cb
absar_portable <saulabraham881@gmail.com>
2025-03-24 13:13:33 -0600

    i started with this project with netbeans

---
+++ Backend/src/main/java/com/ab7ec/backend/Backend.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ab7ec.backend;
+
+/**
+ *
+ * @author absar
+ */
+public class Backend {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
